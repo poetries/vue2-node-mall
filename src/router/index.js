0f2c961c17a5432cafe0e7db55c9d491d6bf86a3
poetries <jingguanliuye@gmail.com>
2017-10-26 18:53:41 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,9 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Hello',
+        component: HelloWorld
+    }]
 })
